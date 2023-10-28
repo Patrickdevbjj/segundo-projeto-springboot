b7ec61f6309a974bf5c45c8b841fd45bda131863
Patrick Rodrigues <patrickdevbjj6@hotmail.com>
2023-10-28 00:56:25 -0300

    new methods

--- segundo-projeto-springboot/src/main/java/br/com/patrick/controller/PrimeiraController.java
+++ segundo-projeto-springboot/src/main/java/br/com/patrick/controller/PrimeiraController.java
@@ -1,15 +1,57 @@
 package br.com.patrick.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
     
-    @GetMapping("/primeiroMetodo")
-    public String primeiroMetodo() {
-        return "Meu primeiro metodo API rest";
+    @GetMapping("/primeiroMetodo/{id}")
+    public String primeiroMetodo(@PathVariable String id) {
+        return "O parametro é " +id;
     }    
-}
+
+    @GetMapping("/metodoComQueryParams")
+    public String metodoComQueryParams(@RequestParam String id) {
+        return "O parametro com metodoComQueryParams é " +id;
+    }
+
+    @GetMapping("/metodoComQueryParams2")
+    public String metodoComQueryParams2(@RequestParam Map< String, String> allParams) {
+        return "O parametro com metodoComQueryParams2 é " + allParams.entrySet();
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name ) {
+    return  "metodoComHeaders" + name;
+     
+    }    
+    
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+    return "metodoComHeaders" + headers.entrySet(); 
+    }
+    
+    @GetMapping("/metodoResponseEntity/{id}")
+    public ResponseEntity<Object> metodoResponseEntity(@PathVariable Long id) {
+        var usuario = new Usuario("danieleleao");
+        if (id > 5 ) {
+            return ResponseEntity.status(HttpStatus.OK).body("Mensagem de erro");
+        } 
+        return ResponseEntity.badRequest().body("Numero menor que 5");
+    }
+    
+    record Usuario(String username) {}
+
+}   
